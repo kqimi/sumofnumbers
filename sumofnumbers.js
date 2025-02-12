e7212ccde0412f0723647f8edcac3bfec259e8be
Kimi Li <qianbing@hawaii.edu>
2025-02-12 13:05:45 -1000

    in class practice wod

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -7,6 +7,7 @@ function sumFor(numbers) {
     return sum;
 }
 console.log(sumFor([1, 2, 3, 4])); // 10
+
 function sumWhile(numbers) {
     let sum = 0;
     let i = 0;
@@ -17,6 +18,7 @@ function sumWhile(numbers) {
     return sum;
 }
 console.log(sumWhile([1, 2, 3, 4])); // 10
+
 function sumRecursion(numbers) {
     if (numbers.length === 0) {
         return 0;
@@ -24,6 +26,7 @@ function sumRecursion(numbers) {
     return numbers[0] + sumRecursion(numbers.slice(1));
 }
 console.log(sumRecursion([1, 2, 3, 4])); // 10
+
 function sumTheFunctionalWay(numbers) {
     return numbers.reduce((acc, cur) => acc + cur, 0);
 }
